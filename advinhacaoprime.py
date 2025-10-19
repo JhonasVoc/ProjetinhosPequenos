0c23f1bb5b6f5715ab5a41b39e9d6c620f41c744
Jhonas Vinicius Oliveira Camili <jhonasviniciusoliveiracamili@MacBook-Air-de-Jhonas.local>
2025-10-19 11:28:36 -0300

    finalizando adivinhacao

--- advinhacaoprime.py
+++ advinhacaoprime.py
@@ -1,7 +1,6 @@
 import random
 
-
-def gerar_numero():
+def gerar_numero(min,max):
     num = random.randint(min,max)
     return num
 
@@ -9,7 +8,7 @@ def gerar_numero():
 def receber_chute(min,max):
     while True:
         try:   
-            chute = int(input("Digite seu chute (entre 0 e 100): "))
+            chute = int(input(f"Digite seu chute entre {min} e {max}.: "))
             if chute < min or chute >max:
                 print(f"Por favor, digite um número entre {min} e {max}.")
             else:
@@ -23,7 +22,7 @@ def selecao_dificuldade():
     print("1 - Facil (intervalo de advinhação 0-25/5 dicas)")
     print("2 - Médio (intervalo de advinhação 0-50/4 dicas)")    
     print("3 - Dicifil(intervalo de advinhação 0-75/3 dicas)")  
-    print("4 - Mega HardCore rock and rollDicifil(intervalo de advinhação 0-125/2 dicas)")           
+    print("4 - Mega HardCore rock and roll Dicifil(intervalo de advinhação 0-125/2 dicas)")           
     while True:                                
         escolha = int(input("Escolha a dificuldade desejada(1-4): "))
         if escolha == 1:
@@ -56,54 +55,69 @@ def selecao_dificuldade():
 def conferir_jogo(num,chute,dicas):
     count = 0
     tentativas = 0
+    lista_dicas = list(range(1,6))# gerando lista com opçoes que podem ser escolhidas nas dicas
     while num != chute:
         if count <= dicas:
             print(f"Aqui vai sua dica n'{count +1}")
-            print(f"xxxxx dica")
+            dica_escolhida = random.choice(lista_dicas)
+            gerar_dicas(num,chute,dica_escolhida)
+            lista_dicas.remove(dica_escolhida)
             count += 1
             tentativas +=1
             chute = int(input("Digite seu chute novamente: "))
+            print()
         else:
             print("Você acertou!!!!")
-            print(f"Tentativas usadas:{tentativas}")
-            print(f"Dicas usadas{count}")
+            print(f"Tentativas usadas: {tentativas}")
+            print(f"Dicas usadas: {count}")
             break
 
 
 
-def gerar_dicas(num,chute):
-        if abs(num,chute) <= 10:
-            print("A diferença entre chute e o numero é menor ou igual a 10")
-        else:
-            print("A diferença entre chute e o numero é maior ou que 10")
 
-        if num % 2 == 0:
-            print("O numero é par")
-        else:
-            print("O numero é impar")
+def gerar_dicas(num,chute,id_dica):
+        if id_dica == 1:
+            if abs(num - chute) <= 10:
+                print("A diferença entre chute e o numero é menor ou igual a 10")
+            else:
+                print("A diferença entre chute e o numero é maior ou que 10")
 
-        if num % 10 == 0:
-            print("O número é divisivel por 10")
-        else:
-            print("O número não é divisivel por 10")
+        if id_dica ==2:
+            if num % 2 == 0:
+                print("O numero é par")
+            else:
+                print("O numero é impar")
 
-        if num < 10:
-            print("O número é menor que 10")
-        else:
-            print("O número é menor que 10")
+        if id_dica == 3:
+            if num % 10 == 0:
+                print("O número é divisivel por 10")
+            else:
+                print("O número não é divisivel por 10")
+        if id_dica ==4 :
+            if num < 10:
+                print("O número é menor que 10")
+            else:
+                print("O número é menor que 10")
 
-        if chute<num:
-            print("O chute é menor que o numero")
-        else:
-            print("O chute é maior que o numero")
+        if id_dica ==5:
+            if chute<num:
+                print("O chute é menor que o numero")
+            else:
+                print("O chute é maior que o numero")
             
     
+def main():
+    print("Bem vindo ao jogo da advinhaçao hard mega nice")
+    min,max,dicas = selecao_dificuldade()
+    print("Gerando seu númerooo misterioso....")
+    num = gerar_numero(min,max)
+    chute = receber_chute(min,max)
+    conferir_jogo(num,chute,2)
 
-gerar_dicas(2,3)
+
+main()    
 
 
 
-#min,max, dicas =selecao_dificuldade()
-#print(f"{min}. {max}. {dicas}. ")
 
 
